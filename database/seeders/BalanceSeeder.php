97934aa508330c8efa1db0b4435dd70da2271bea
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-01-02 03:39:47 +0700

    defining balance seeder

--- database/seeders/BalanceSeeder.php
+++ database/seeders/BalanceSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Balance;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class BalanceSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class BalanceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Balance::factory()->count(13)->create();
     }
 }
