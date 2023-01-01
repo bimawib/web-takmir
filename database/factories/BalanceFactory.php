6d45b3a4f455da8337a6e9da46277a743e830a54
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-01-02 03:12:29 +0700

    generating factories for balance model

--- database/factories/BalanceFactory.php
+++ database/factories/BalanceFactory.php
@@ -16,8 +16,26 @@ class BalanceFactory extends Factory
      */
     public function definition()
     {
+        $number_rand = rand(0,1);
+
+        if($number_rand == 0){
+            $spend_rand = $this->faker->numberBetween(5000,500000);
+            $income_rand = 0;
+            $is_spend_rand = 1;
+        } else {
+            $spend_rand = 0;
+            $income_rand = $this->faker->numberBetween(5000,500000);
+            $is_spend_rand = 0;
+        }
+
         return [
-            //
+            'user_id'=>$this->faker->unique()->numberBetween(1,3),
+            'name'=>fake()->word(),
+            'is_spend'=>$is_spend_rand,
+            'spend_balance'=>$spend_rand,
+            'incoming_balance'=>$income_rand,
+            'total_balance'=>$this->faker->numberBetween(500000,5000000),
+            'note'=>$this->faker->text(13)
         ];
     }
 }
