805790bf37830158ec61281c39bc7378e87602f4
Bima Sahidsyah Putra Wibowo <bimaencun@gmail.com>
2023-01-05 04:01:42 +0700

    adding coment

--- app/Filters/V1/AgendaFilter.php
+++ app/Filters/V1/AgendaFilter.php
@@ -14,6 +14,7 @@ class AgendaFilter extends ApiFilter {
     // protected $columnMap = [
     //     'agendaDate'=>'agenda_date'
     // ];
+    // this is for a query that has different typing than the column name in DB
 
     protected $operatorMap = [
         'eq'=>'=',
